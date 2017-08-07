1dcc52e5959bdf52f4fea3d5d641b68abf0da6cf
jacob ruleaux and max scher <max@>
2017-08-07 14:50:28 -0700

    debug to remove redundant display and find update btn

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -1,21 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    isImageShowing: false,
-    actions: {
+  isImageShowing: false,
+  actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
     },
     imageHide: function() {
       this.set('isImageShowing', false);
-    },
-    update(rental, params) {
-     this.sendAction('update', rental, params);
-   },
-    delete(rental) {
-      if (confirm('Are you sure you want to delete this rental?')) {
-        this.sendAction('destroyRental', rental);
-      }
     }
   }
 });
